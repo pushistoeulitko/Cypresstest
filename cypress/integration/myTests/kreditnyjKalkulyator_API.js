648699f179fe54948c305d9af01448140c65d2ae
pushistoeulitko <pushistoeulitko@yandex.ru>
2020-08-24 20:47:10 +0500

    предпоследние попытки

--- cypress/integration/myTests/kreditnyjKalkulyator_API.js
+++ cypress/integration/myTests/kreditnyjKalkulyator_API.js
@@ -1,6 +1,33 @@
 
 describe('API tests', () => {
+    Cypress.config('baseUrl','https://fincalculator.ru')
+
     it('ApI - 1', () => {
-       cy.request('GET', 'https://fincalculator.ru/kreditnyj-kalkulyator')
-      })
-})  
+        cy.request('./kreditnyj-kalkulyator').then((response) => {
+            expect(response).to.have.property('status', 200)
+        })
+    })
+
+    it('ApI - 0', () => {
+        cy.fixture('credit').then(credit => {
+            cy.request('POST', 'calculators/CalculateLoan', credit ).then((response) => {
+                expect(response.status).to.eq(200)
+                expect(response.body).to.have.property('amount',credit.amount)
+                expect(response.body).to.have.property('period',credit.period)
+                expect(response.body).to.have.property('rate',credit.rate)
+                // expect(response.body).to.deep.equal('amount', credit.amount)
+                // expect(response.body).to.deep.equal('period',credit.period)
+                // expect(response.body).to.deep.equal('rate',credit.rate)
+
+            })
+        })
+    })
+
+    it('ApI - 2', () => {
+        const item = {"amount": "7000000"}
+        cy.request('POST', '/calculators/CalculateLoan', item)
+            .its('body')
+            //.its('data')
+            .should('deep.equal', item)
+    })
+})
--- cypress/integration/myTests/kreditnyjKalkulyator_UI.js
+++ cypress/integration/myTests/kreditnyjKalkulyator_UI.js
@@ -1,37 +1,30 @@
-/// <reference types="cypress" />
 
 context('kreditnyjKalkulyator', () => {
   beforeEach(() => {
-    cy.visit('https://fincalculator.ru/kreditnyj-kalkulyator') })
+    cy.visit('https://fincalculator.ru/kreditnyj-kalkulyator')
+  })
 
-  //проверка заголовка
+  //проверка заголовка страницы "Кредитный калькулятор"
   it('UI - 1', () => {
-      cy.title().should('eq','Кредитный калькулятор')
-    })
+    cy.title().should('eq', 'Кредитный калькулятор')
+  })
 
-  //проверка поля суммы
+  //чекбокс "учитывать инфляцию"
   it('UI - 2', () => {
-    cy.get('input.all-column12[name="loanParameters.amount"]').contains('1 500 000,00') 
-  })
-
-  //проверка ввода не валидного значения в поле суммы
-  it('UI - 3', () => {
-      cy.get('input.all-column12[name="loanParameters.amount"]')
-      .type('djfdkghdkgdk')
-      .should('eq','1500000,00')
-  })
-
-  //чекбокс учитывать инфляцию
-  it('UI - 4', () => {
     cy.get('input.pull-right[name="inflationParameters.isDiscounted"]')
-      .not('[disabled]')
-      .check()
-      .should('be.checked')
+        .not('[disabled]')
+        .check()
+        .should('be.checked')
+    cy.get('button:contains("Рассчитать")').not('[disabled]').click()
+    cy.get('span.pull-left[data-bind="text : decimalToString(inflationTotalAmount)"]').should('be.visible')
+    cy.get('div:nth-child(6) > div.all-column6.result-value.inflation > span').should('be.visible')
   })
 
-  //кликнуть на кнопку "Рассчитать"
-  it('UI - 5', () => {
+  // Ввести и рассчитать
+  it('UI - 3', () => {
+    cy.get('input.all-column12[name="loanParameters.amount"]').type('3000000')
     cy.get('button:contains("Рассчитать")').not('[disabled]').click()
+    cy.get('span.pull-left[data-bind="text : decimalToString(totalAmount)"]').should('be.visible')
   })
 
 })
